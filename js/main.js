a378fd5e6d29f1f5a88ad0e07bdc77c79c66c40b
Paul Park <paulpark330@hotmail.com>
2021-07-28 12:45:01 -0700

    modified getAPI function

--- js/main.js
+++ js/main.js
@@ -4,39 +4,26 @@ var $intro = document.querySelector('.intro');
 
 var allChampions = [];
 
-var urlOne =
-  'https://api.pandascore.co/lol/champions?token=_g8je6_IZ8WrEPjlCgB-PG2588hD3Ume9yISokmfKl3uP-h5gGc&page[size]=100&page[number]=1';
-var urlEncodedOne = encodeURIComponent(urlOne);
-var finalUrlOne = 'https://lfz-cors.herokuapp.com/?url=' + urlEncodedOne;
-
-var urlTwo =
-  'https://api.pandascore.co/lol/champions?token=_g8je6_IZ8WrEPjlCgB-PG2588hD3Ume9yISokmfKl3uP-h5gGc&page[size]=100&page[number]=2';
-var urlEncodedTwo = encodeURIComponent(urlTwo);
-var finalUrlTwo = 'https://lfz-cors.herokuapp.com/?url=' + urlEncodedTwo;
-
-function getAPI(finalUrlOne, finalUrlTwo) {
-  var xhr1 = new XMLHttpRequest();
-  xhr1.open('GET', finalUrlOne);
-  xhr1.responseType = 'json';
-  xhr1.addEventListener('load', function () {
-    for (let i = 0; i < xhr1.response.length; i++) {
-      allChampions.push(xhr1.response[i]);
+function getAPI(url) {
+  var urlEncodedOne = encodeURIComponent(url);
+  var finalUrlOne = 'https://lfz-cors.herokuapp.com/?url=' + urlEncodedOne;
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', finalUrlOne);
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', function () {
+    for (let i = 0; i < xhr.response.length; i++) {
+      allChampions.push(xhr.response[i]);
     }
   });
-  xhr1.send();
-
-  var xhr2 = new XMLHttpRequest();
-  xhr2.open('GET', finalUrlTwo);
-  xhr2.responseType = 'json';
-  xhr2.addEventListener('load', function () {
-    for (let i = 0; i < xhr2.response.length; i++) {
-      allChampions.push(xhr2.response[i]);
-    }
-  });
-  xhr2.send();
+  xhr.send();
 }
 
-getAPI(finalUrlOne, finalUrlTwo);
+getAPI(
+  'https://api.pandascore.co/lol/champions?token=_g8je6_IZ8WrEPjlCgB-PG2588hD3Ume9yISokmfKl3uP-h5gGc&page[size]=100&page[number]=1'
+);
+getAPI(
+  'https://api.pandascore.co/lol/champions?token=_g8je6_IZ8WrEPjlCgB-PG2588hD3Ume9yISokmfKl3uP-h5gGc&page[size]=100&page[number]=2'
+);
 
 function renderChamps() {
   var alphChamps = allChampions.sort((a, b) => (a.name > b.name ? 1 : -1));
