2de742cbed0530a0f0c9e6e513cf3e5a124094ed
Paul Park <paulpark330@hotmail.com>
2021-07-29 15:39:00 -0700

    User is able to add champion to the comparison list

--- js/main.js
+++ js/main.js
@@ -2,6 +2,11 @@ var $championList = document.querySelector('#champion-list');
 var $start = document.querySelector('.get-started');
 var $intro = document.querySelector('.intro');
 var $search = document.querySelector('.search');
+var $comparisonList = document.querySelector('.comparison-list');
+var $navFoot = document.querySelector('.nav-foot');
+var $championContainer = document.querySelector('.champion-container');
+var $comparisonContainer = document.querySelector('.comparison-container');
+
 var $champImg = [];
 var allChampions = [];
 
@@ -37,7 +42,7 @@ function renderChamps() {
     div.className = 'champ-img';
     overlay.className = 'overlay';
     div.setAttribute('champ-name', alphChamps[i].name.toLowerCase());
-    plus.setAttribute('src', '../images/Plus.png');
+    plus.setAttribute('src', 'images/plus.png');
     plus.className = 'plus';
     div.appendChild(img);
     div.appendChild(overlay);
@@ -50,12 +55,7 @@ function renderChamps() {
   $champImg = document.querySelectorAll('.champ-img');
 }
 
-$start.addEventListener('click', renderChamps);
-
-$search.addEventListener('input', searchChamps);
-
 function searchChamps(event) {
-
   for (let i = 0; i < $champImg.length; i++) {
     var searchString = $search.value.toLowerCase();
     if ($champImg[i].getAttribute('champ-name').includes(searchString)) {
@@ -65,3 +65,46 @@ function searchChamps(event) {
     }
   }
 }
+
+function addChamps(event) {
+  var target = event.target.parentNode.parentNode.getAttribute('champ-name');
+  for (let i = 0; i < allChampions.length; i++) {
+    if (target === allChampions[i].name.toLowerCase()) {
+      var img = document.createElement('img');
+      var div = document.createElement('div');
+      var overlay = document.createElement('div');
+      var remove = document.createElement('img');
+      img.setAttribute('src', allChampions[i].big_image_url);
+      div.className = 'comp-img';
+      div.setAttribute('champ-name', allChampions[i].name.toLowerCase());
+      div.appendChild(img);
+      overlay.className = 'overlay-remove';
+      remove.setAttribute('src', 'images/remove.png');
+      overlay.appendChild(remove);
+      div.appendChild(overlay);
+      $comparisonList.appendChild(div);
+    }
+  }
+}
+
+function switchView(event) {
+  var dataView = event.target.getAttribute('data-view');
+  if (dataView === 'champion') {
+    $championContainer.className = 'champion-container';
+    $comparisonContainer.className = 'comparison-container hidden';
+    $search.className = 'search';
+  } else if (dataView === 'comparison') {
+    $championContainer.className = 'champion-container hidden';
+    $comparisonContainer.className = 'comparison-container';
+    $search.className = 'search hidden';
+  }
+
+}
+
+$start.addEventListener('click', renderChamps);
+
+$search.addEventListener('input', searchChamps);
+
+$championList.addEventListener('click', addChamps);
+
+$navFoot.addEventListener('click', switchView);
